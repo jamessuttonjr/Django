6a39faf7375bd5269f535dc7a8564efa777d899d
jamessuttonjr <james_suttonjr1@baylor.edu>
2021-12-01 15:23:08 -0600

    Created hyperlinks for topics and gave date and text descriptions

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     return render(request, 'MainApp/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+
+    context = {'topics':topics}   
+# the key is the variable used in the template file or the html file and the value of the dictionary is the variable used in the view function. WILL BE A QUESTION ON INTERVIEW
+    
+    return render(request, 'MainApp/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.all()
+
+    context = context = {'topic':topic, 'entries':entries} 
+
+    return render(request, 'MainApp/topic.html', context)
